a14c247eca6baf3ded422051b733f0285b77dcf3
unknown <greenjm@GREENJM-1.rose-hulman.edu>
2015-02-28 00:13:12 -0500

    Testing database

---
+++ web/setdb.php
@@ -0,0 +1,19 @@
+<?php
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$dbname = substr($url["path"], 1);
+
+	$db = new PDO("mysql:dbname=$dbname;host=$server", $username, $password);
+
+	$query = $db->prepare('create table user (userid int, username varchar(255), email varchar(255), first-name varchar(255), last-name varchar(255), pebble-id varchar(255)');
+	$query->execute();
+
+	$insert = $db->prepare('insert into user (id, username, email, first-name, last-name, pebble-id) values (1, greenjm, greenjm_rose-hulman.edu, Josh, Green, qwerty');
+	$insert->execute();
+
+	$select = $db->prepare('select * from user');
+	$select->execute();
+?>
